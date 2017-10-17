77981380a0b407f1b80b9ec1409da347cedc0f1e
Gibson <gibsond@msjmath.loc>
2017-10-17 10:52:38 -0400

    added environment vars

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var bodyParser = require("body-parser");
 var mysql = require("mysql");
 var bcrypt = require("bcryptjs");
 
+console.log(process.env.DBHOST);
 var connection = mysql.createConnection({
-    host:DBHOST,
-    user: DBUSER,
-    password:DBPASSWORD,
-    database: DB
+    host:process.env.DBHOST,
+    user: process.env.DBUSER,
+    password:process.env.DBPASSWORD,
+    database: process.env.DB
 });
 var port = process.env.PORT || 8080;
 
@@ -23,6 +24,18 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(session({secret: "**Dennis is cool*??",resave:true,saveUninitialized:true}));
+
+connection.query("select * from user",function(err,results){
+    if(err)
+    {
+        console.log("error",err);
+    }
+    else
+    {
+        console.log("results",results)
+    }
+
+});
 
 app.listen(port, () => {
     console.log("Server is up on port : " + port);
